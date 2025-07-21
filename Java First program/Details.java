98d27ae21f430d0d67e213295d96b007f0c75414
Rohit Kumar <144145989+Rohit57915@users.noreply.github.com>
2025-07-21 13:10:14 +0530

    Add files via upload

---
+++ Java First program/Details.java
@@ -0,0 +1,12 @@
+class Details
+{
+public static void main(String[] args)
+{
+System.out.println("Rohit Kumar");
+System.out.println(2024);
+System.out.println(7.14);
+System.out.println('m');
+System.out.println(true);
+System.out.println(false);
+}
+}
---
+++ Java First program/GoodFriend.java
@@ -0,0 +1,7 @@
+class GoodFriend
+{
+public static void main (String[] args)
+{
+System.out.println(" Uday Kumar");
+}
+}
